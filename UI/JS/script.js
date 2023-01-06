4a1c4b8177459ef9a325a73bc2d5d402ea7588a1
kagame8732 <kagame8732@gmail.com>
2023-01-06 10:42:30 +0200

    ui

--- UI/JS/script.js
+++ UI/JS/script.js
@@ -5,3 +5,18 @@ menu_btn.addEventListener("click", function () {
   menu_btn.classList.toggle("is-active");
   mobile_menu.classList.toggle("is-active");
 });
+
+let state = false;
+
+function toggle() {
+  if (state) {
+    document.getElementById("password").setAttribute("type", "password");
+    document.getElementById("eye").style.color = "#207733";
+    state = false;
+  } else {
+    document.getElementById("password").setAttribute("type", "text");
+    document.getElementById("eye").style.color = "#000000";
+
+    state = true;
+  }
+}
