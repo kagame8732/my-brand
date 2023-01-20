f2ed63661a769302d2f381f978d9f860b26c440c
kagame8732 <kagame8732@gmail.com>
2023-01-20 16:09:46 +0200

    local storage

--- UI/JS/script.js
+++ UI/JS/script.js
@@ -5,7 +5,6 @@ const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
 });
-//Active navbar
 
 //Password visible and invisible
 let state = false;
@@ -21,87 +20,95 @@ function toggle(elementId, eye) {
   }
 }
 //Blog menu
-// document.getElementById("add-item").addEventListener("click", function () {
-//   localStorage.setItem(
-//     "name",
-//     JSON.stringify({ name: "Kagame", food: "Cheese" })
-//   );
-//   updateUI();
-// });
+let blogCards = document.getElementById("blog-cards");
+let blogMessages = JSON.parse(localStorage.getItem("blogInfo")) || [];
 
-const blogForm = document.getElementById("blogForm");
-const blogKey = "blogFormData";
-let blogData = JSON.parse(localStorage.getItem(blogKey)) || [];
-console.log(blogData);
-blogForm.addEventListener("submit", (e) => {
+document.getElementById("blog-form").addEventListener("submit", function (e) {
   e.preventDefault();
-  const blogFormValues = {
-    title: blogForm.blogTitle.value,
-    description: blogForm.blogMessage.value,
-  };
-  blogData.push(blogFormValues);
-  localStorage.setItem(blogKey, JSON.stringify(blogData));
-  alert("Blog added successfull");
-  blogForm.blogImage.value = "";
-  blogForm.blogTitle.value = "";
-  blogForm.blogMessage.value = "";
-});
-function updateUI() {
-  let values = [],
-    keys = Object.keys(localStorage),
-    i = keys.length;
-  while (i--) {
-    values.push(localStorage.getItem(keys[i]));
-  }
-  document.getElementById("list-heading").textContent = values;
-}
+  let title = document.getElementById("blogTitle");
+  let message = document.getElementById("blogMessage");
+  let comment = document.getElementById("blogComment");
 
+  let blogInfo = {
+    title: title.value,
+    message: message.value,
+    comment: comment.value,
+    index: blogMessages.length + 1,
+  };
+  blogMessages = [...blogMessages, blogInfo];
+  localStorage.setItem("blogInfo", JSON.stringify(blogMessages));
+  title.value = "";
+  message.value = "";
+  comment.value = "";
+});
+
+const blog = blogMessages.map((item) => {
+  console.log(item);
+});
 // Blog image
-document.querySelector("#blogImage").addEventListener("change", function () {
-  const reader = new FileReader();
-  reader.addEventListener("load", () => {
-    localStorage.setItem("Image", reader.result);
-  });
-  reader.readAsDataURL(this.files[0]);
-});
-document.addEventListener("DOMContentLoaded", () => {
-  const recentImageDataUrl = localStorage.getItem("Image");
-  if (recentImageDataUrl) {
-    document
-      .querySelector("#imgPreview")
-      .setAttribute("src", recentImageDataUrl);
-  }
-});
+
 // Contact
-const form = document.getElementById("myForm");
-const contactKey = "ContactFormData";
-let contactData = JSON.parse(localStorage.getItem(contactKey)) || [];
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const contactFormValues = {
-    name: form.name.value,
-    email: form.email.value,
-    messages: form.description.value,
-  };
-  contactData.push(contactFormValues);
-  localStorage.setItem(contactKey, JSON.stringify(contactData));
-  alert("Added successfull");
-  form.name.value = "";
-  form.email.value = "";
-  form.description.value = "";
-});
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    let name = document.getElementById("name");
+    let email = document.getElementById("email");
+    let contactMessage = document.getElementById("description");
+    let contactMessages = JSON.parse(localStorage.getItem("contactInfo")) || [];
+
+    let message = {
+      name: name.value,
+      email: email.value,
+      contactMessage: contactMessage.value,
+      index: contactMessages.length + 1,
+    };
+    contactMessages = [...contactMessages, message];
+    localStorage.setItem("contactInfo", JSON.stringify(contactMessages));
+    name.value = "";
+    email.value = "";
+    contactMessage.value = "";
+  });
+
 // Login
-const loginForm = document.getElementById("loginForm");
-const loginKey = "loginFormData";
-let loginData = JSON.parse(localStorage.getItem(loginKey)) || [];
-loginForm.addEventListener("submit", (e) => {
+document.getElementById("login-form").addEventListener("submit", function (e) {
   e.preventDefault();
-  const loginFormValues = {
-    email: loginForm.loginEmail.value,
-    password: loginForm.loginPassword.value,
+  let email = document.getElementById("login-email");
+  let password = document.getElementById("loginPassword");
+  let users = JSON.parse(localStorage.getItem("userInfo")) || [];
+
+  let userInfo = {
+    email: email.value,
+    password: password.value,
+    index: users.length + 1,
   };
-  loginData.push(loginFormValues);
-  localStorage.setItem(loginKey, JSON.stringify(loginData));
-  loginForm.loginEmail.value = "";
-  loginForm.loginPassword.value = "";
+  users = [...users, userInfo];
+  localStorage.setItem("userInfo", JSON.stringify(users));
+  email.value = "";
+  password.value = "";
+});
+
+//Signup
+
+document.getElementById("signup-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  let firstName = document.getElementById("firstName");
+  let lastName = document.getElementById("lastName");
+  let signupEmail = document.getElementById("signup-email");
+  let signupPassword = document.getElementById("signup-password");
+  let signupUsers = JSON.parse(localStorage.getItem("signupInfo")) || [];
+
+  let signupInfo = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    signupEmail: signupEmail.value,
+    signupPassword: signupPassword.value,
+    index: signupUsers.length + 1,
+  };
+  signupUsers = [...signupUsers, signupInfo];
+  localStorage.setItem("signupInfo", JSON.stringify(signupUsers));
+  firstName.value = "";
+  lastName.value = "";
+  signupEmail.value = "";
+  signupPassword.value = "";
 });
